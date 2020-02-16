49a5bca46659b36aca1c6cdd60aef8bcb5babd69
Dimitrios Piliouras <jimpil1985@gmail.com>
2020-02-16 15:46:35 +0000

    0.2.0 released - prepare for next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jedi-time "0.2.0"
+(defproject jedi-time "0.2.1-SNAPSHOT"
   :description "Datafiable/Navigable protocol extensions for the core java.time objects."
   :url "https://github.com/jimpil/jedi-time"
   :author "Dimitrios Piliouras <jimpil1985@gmail.com>"
